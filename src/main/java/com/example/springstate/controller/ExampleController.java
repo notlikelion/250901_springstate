ce6d73ac3ed969eacc5756293def6439189678d0
notlikelion <notlikelion@gmail.com>
2025-09-01 10:05:06 +0900

    form

--- src/main/java/com/example/springstate/controller/ExampleController.java
+++ src/main/java/com/example/springstate/controller/ExampleController.java
@@ -3,6 +3,10 @@ package com.example.springstate.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
 
 @Controller
 public class ExampleController {
@@ -14,5 +18,17 @@ public class ExampleController {
         // ${userName}
         model.addAttribute("userName", "모델");
         return "hello"; // src/main/resources/templates/hello.html
+    }
+
+    @PostMapping("/save-data")
+    public String saveData(@RequestParam String data, Model model) {
+        Random rd = new Random();
+        boolean success = rd.nextBoolean(); // 반반 확률
+        if (success) {
+            model.addAttribute("message", data + "가 성공적으로 저장되었습니다.");
+        } else {
+            model.addAttribute("message", data + "의 저장에 실패했습니다.");
+        }
+        return "result";
     }
 }
